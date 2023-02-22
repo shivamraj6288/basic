23c9600aadbfe48a6d8785c7fe031d2a1bb8d543
shivamraj6288 <shivamraj.study@gmail.com>
2023-02-22 14:09:12 +0530

    added ec2 dns name

--- basic/settings.py
+++ basic/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&a0jdo&0qci0)e^xry50yzh$03$l1bw%&$s)-)t=@-qokqxi(a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-3-7-66-37.ap-south-1.compute.amazonaws.com']
 
 CORS_ORIGIN_ALLOW_ALL=True
 
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
